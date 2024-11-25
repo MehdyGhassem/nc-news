84ab44640467a36f307e311d217183f16fc05e9a
Mehdy Ghassem <mehdy.ghassem@aol.com>
2024-11-25 16:47:07 +0000

    Implement GET /api/topics endpoint with tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -15,3 +15,20 @@ describe("GET /api", () => {
       });
   });
 });
+
+describe("GET /api/topics", () => {
+  test("200: Responds with an array of topic objects", () => {
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then(({ body: { topics } }) => {
+        expect(topics.length).toBe(3);
+        expect(topics[0]).toEqual({
+          slug: expect.any(String),
+          description: expect.any(String)
+        });
+      });
+  });
+});
+
+
