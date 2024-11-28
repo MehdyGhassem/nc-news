106132f1b48d1330aebdb9797d373890f9caba0c
Mehdy Ghassem <mehdy.ghassem@aol.com>
2024-11-28 16:22:41 +0000

    Fix articles and comments API

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const {getEndpoints, getTopics} = require('./controllers/topics.controller');
 const {getArticleById, getArticles} = require('./controllers/articles.controller');
+const {getCommentsByArticleId} = require('./controllers/comments.controller')
 
 const app = express();
 
@@ -10,10 +11,26 @@ app.get('/api', getEndpoints);
 app.get('/api/topics', getTopics);
 app.get('/api/articles/:article_id', getArticleById);
 app.get('/api/articles', getArticles)
+app.get('/api/articles/:article_id/comments', getCommentsByArticleId);
 
 app.use('*', (req, res) => {
     res.status(404).send({ msg: 'Not Found' });
   });
+
+  app.use((err, req, res, next) => {
+    if (err.status && err.msg) {
+      return res.status(err.status).send({ msg: err.msg });
+    }
+    next(err); 
+  });
+  
+  app.use((err, req, res, next) => {
+    if (err.code === '22P02') { 
+      return res.status(400).send({ msg: 'Invalid article_id' });
+    }
+    next(err); 
+  });
+
   
 app.use((err, req, res, next) => {
     if (err.status && err.msg) {
