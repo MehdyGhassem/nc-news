2643d93515a10a2e9feda8feeb1b659a2f7b8c65
Mehdy Ghassem <mehdy.ghassem@aol.com>
2024-11-28 13:48:50 +0000

    Fix listen.js to use port 10000

--- listen.js
+++ listen.js
@@ -1,5 +1,5 @@
 const app = require('./app')
-const port = process.env.PORT || 9090;
+const port = process.env.PORT || 10000
 
 app.listen(port, '0.0.0.0', () => {
     console.log(`Listening on port ${port}...`);
