8c704a62d17b0f5b4106d415cfe01ae3eb4b894f
Mehdy Ghassem <mehdy.ghassem@aol.com>
2024-11-28 11:06:27 +0000

    Set up hosting with Supabase and Render

--- db/connection.js
+++ db/connection.js
@@ -1,15 +1,19 @@
 const { Pool } = require('pg');
 const ENV = process.env.NODE_ENV || 'development';
 
-if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-  throw new Error("PGDATABASE or DATABASE_URL not set");
-}
+require('dotenv').config({
+  path: `${__dirname}/../.env.${ENV}`,
+});
 
-const config = {};
+const config = {}
+
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error('PGDATABASE not set or DATABASE_URL not set"');
+}
 
 if (ENV === "production") {
   config.connectionString = process.env.DATABASE_URL;
-  config.max = 2; 
+  config.max = 2;
 }
 
 module.exports = new Pool(config);
